3c33f5a37ba3bde7038b91d42d4ed612f2d44316
Balazs Cseh <balazs.cseh@arcadiagroup.co.uk>
2016-10-18 10:37:13 +0100

    added all apps to cask installs, error handling, extended APM installs

---
+++ apm-install.sh
@@ -0,0 +1,22 @@
+
+apm install \
+	autoclose-html \ 
+	autocomplete-paths \
+	editorconfig \
+	emmet \
+	expose \
+	git-time-machine \
+	highlight-selected \ 
+	language-gherkin \ 
+	react \
+	relative-numbers \
+	run \
+	split-diff \
+	pretty-json \ 
+	pigments \ 
+	local-history \ 
+	linter \ 
+	linter-eslint \ 
+	file-icons \
+	minimap \
+	minimap-highlight-selected
---
+++ install.sh
@@ -0,0 +1,46 @@
+
+#################################
+# Main Install Script
+# Installs all apps I use daily
+#################################
+
+# INSTALL HOMEBREW
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
+
+# INSTALL CASK FOR GETTING ALL APPS
+brew tap caskroom/cask &&
+brew install cask
+
+
+# INSTALL FUNCTION FOR ERROR REPORTING
+ERRORS=()
+function install {
+    brew cask install $1
+    RC=$?
+
+    if [ $RC -ne 0]; then
+        echo "Could not install $1" >> installs.log
+        ERRORS+=($RC)
+    fi
+}
+
+
+# INSTALL ALL APPS
+install atom &
+install google-chrome &
+install iterm2 &
+install evernote &
+install slack &
+install spotify &
+install alfred &
+install docker &
+install flycut &
+install seil &
+install skype &
+install virtualbox &&
+
+if [ ${#ERRORS[@]} -gt 0 ]; then
+    echo "Could not install ALL apps successfully. Check installs.log for error reports."
+fi
+
+echo "Don't forget to add Chrome extensions (vimium) and configure Seil to replace CapsLock"
