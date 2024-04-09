b9da09cef3f121d362985805defc8cd37f3e58cf
Balazs Cseh <balazs.cseh89@gmail.com>
2024-04-09 12:07:33 +0100

    feat: js toolchain installs

---
+++ toolchains/js/install.sh
@@ -0,0 +1,12 @@
+
+# Node
+## Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+## Install Latest LTS Node
+nvm install --lts
+
+## Install Yarn and other package managers
+corepack enable yarn
+
+# TODO: Maybe eventually move all the above to Volta
