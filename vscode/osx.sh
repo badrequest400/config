3990e50e2be5c7da68f4c38a1abb74d5d744fd1c
Balazs Cseh <balazs.cseh89@gmail.com>
2025-02-11 09:47:32 +0000

    feat: turn off press and hold for Cursor

--- vscode/osx.sh
+++ vscode/osx.sh
@@ -1,3 +1,11 @@
 ## Turns off Press and Hold accent letter context menu for VSCode so repeating keys work
+
 echo "Turning off Press and Hold for VSCode"
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+echo "Turning off Press and Hold for Cursor"
+cursor_id=$(osascript -e 'id of app "Cursor"')
+
+if [ -n "$cursor_id" ]; then
+defaults write "$cursor_id" ApplePressAndHoldEnabled -bool false
+fi
